2af779f0d223e1fb71fae9f0f7912c9bf48e3d14
Cloud Driven Solutions <ajthompson@clouddrivensolutions.com>
2021-10-21 12:14:50 -0500

    larger video screen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="flex flex-col overflow-hidden">
       <p>BrightSign Test</p>
       <div className="mx-2 mt-4 border-2 border-red-700">
-        <video width="320" height="240">
+        <video width="1366" height="768">
           <source src="tv:brightsign.biz/hdmi" />
         </video>
       </div>
