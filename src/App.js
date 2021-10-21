013696da64013c458db9e74409923408e16099c3
Cloud Driven Solutions <ajthompson@clouddrivensolutions.com>
2021-10-21 11:23:41 -0500

    testing hdmi input code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div>
+    <div className="flex flex-col overflow-hidden">
       <p>BrightSign Test</p>
+      <div className="mx-2 mt-4 border-2 border-red-700">
+        <video width="320" height="240">
+          <source src="tv:brightsign.biz/hdmi" />
+        </video>
+      </div>
     </div>
   );
 }
